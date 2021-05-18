fad20662c5596113b4bdc4cd23a65c4275571101
Diego_Souza <diego.s.costa@fiesc.com.br>
2021-05-18 10:37:12 -0300

    Alteraçoes dos arquivos na posicao do array, aprendendo sobre: criar um array apenas com um tipo de dado, para nao gerar problema futuro

--- ArrayUtils.php
+++ ArrayUtils.php
@@ -1,10 +1,17 @@
-<?php
+<?php declare(strict_types=1); //Esta declaração faz com que os tipos dos argumentos e retornos de função sejam estritos, sem type juggling.
+
+
 
 class ArrayUtils
 {
     public static function remove(string $elemento, array &$array)
     {
-        $posicao = array_search($elemento, $array);
-        unset($array[$posicao]);
+        $posicao = array_search($elemento, $array, true); //adicionando parametro TRUE, para fazer a comparação de tipagem alem do conteudo;
+        if (is_int ($posicao)) {    //is_int -> não deixa type jugglin transformar o falso em 0, mantendo a funcionalidade do codigo
+            unset($array[$posicao]);
+        } else {
+            echo "Não foi encontrada essa posição no Array";
+        }
+        
     }
 }
--- bank.php
+++ bank.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1); //Esta declaração faz com que os tipos dos argumentos e retornos de função sejam estritos, sem type juggling.
 
 require_once 'ArrayUtils.php';
 
@@ -17,7 +17,7 @@ foreach ($correntistasECompras as $new) {
     echo "<p>$new</p>";
 }
 
-ArrayUtils::remove("Luis", $correntistasECompras);
+ArrayUtils::remove("12", $correntistasECompras);
 
 foreach ($correntistasECompras as $new) {
     echo "<p>$new</p>";
